b4d1e08527d5687bb72df761e2277fde28091742
keigomichi <hundredacrewood1165g7@gmail.com>
2022-10-25 23:28:32 +0900

    chore: 第五回のコードを追加

--- main.go
+++ main.go
@@ -6,9 +6,14 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/labstack/echo-contrib/session"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/srinathgs/mysqlstore"
+	"golang.org/x/crypto/bcrypt"
+
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
-	"github.com/labstack/echo/v4"
 )
 
 type City struct {
@@ -17,24 +22,6 @@ type City struct {
 	CountryCode string `json:"countryCode,omitempty"  db:"CountryCode"`
 	District    string `json:"district,omitempty"  db:"District"`
 	Population  int    `json:"population,omitempty"  db:"Population"`
-}
-
-type Country struct {
-	Code           string  `json:"code,omitempty" db:"Code"`
-	Name           string  `json:"name,omitempty" db:"Name"`
-	Continent      string  `json:"continent,omitempty" db:"Continent"`
-	Region         string  `json:"region,omitempty" db:"Region"`
-	SurfaceArea    float64 `json:"surfaceArea,omitempty" db:"SurfaceArea"`
-	IndepYear      int     `json:"indepYear,omitempty" db:"IndepYear"`
-	Population     int     `json:"population,omitempty" db:"Population"`
-	LifeExpectancy float64 `json:"lifeExpectancy,omitempty" db:"LifeExpectancy"`
-	GNP            float64 `json:"gnp,omitempty" db:"GNP"`
-	GNPOld         float64 `json:"gnpOld,omitempty" db:"GNPOld"`
-	LocalName      string  `json:"localName,omitempty" db:"LocalName"`
-	GovernmentForm string  `json:"governmentForm,omitempty" db:"GovernmentForm"`
-	HeadOfState    string  `json:"headOfState,omitempty" db:"HeadOfState"`
-	Capital        int     `json:"capital,omitempty" db:"Capital"`
-	Code2          string  `json:"code2,omitempty" db:"Code2"`
 }
 
 var (
@@ -46,21 +33,123 @@ func main() {
 	if err != nil {
 		log.Fatalf("Cannot Connect to Database: %s", err)
 	}
-
 	db = _db
 
-	e := echo.New()
+	store, err := mysqlstore.NewMySQLStoreFromConnection(db.DB, "sessions", "/", 60*60*24*14, []byte("secret-token"))
+	if err != nil {
+		panic(err)
+	}
 
-	e.GET("/cities/:cityName", getCityInfoHandler)
-	e.POST("/post", addCityInfoHandler)
+	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Use(session.Middleware(store))
+
+	e.GET("/ping", func(c echo.Context) error {
+		return c.String(http.StatusOK, "pong")
+	})
+	e.POST("/login", postLoginHandler)
+	e.POST("/signup", postSignUpHandler)
+
+	withLogin := e.Group("")
+	withLogin.Use(checkLogin)
+	withLogin.GET("/cities/:cityName", getCityInfoHandler)
 
 	e.Start(":4000")
+}
 
+type LoginRequestBody struct {
+	Username string `json:"username,omitempty" form:"username"`
+	Password string `json:"password,omitempty" form:"password"`
+}
+
+type User struct {
+	Username   string `json:"username,omitempty"  db:"Username"`
+	HashedPass string `json:"-"  db:"HashedPass"`
+}
+
+func postSignUpHandler(c echo.Context) error {
+	req := LoginRequestBody{}
+	c.Bind(&req)
+
+	// もう少し真面目にバリデーションするべき
+	if req.Password == "" || req.Username == "" {
+		// エラーは真面目に返すべき
+		return c.String(http.StatusBadRequest, "項目が空です")
+	}
+
+	hashedPass, err := bcrypt.GenerateFromPassword([]byte(req.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return c.String(http.StatusInternalServerError, fmt.Sprintf("bcrypt generate error: %v", err))
+	}
+
+	// ユーザーの存在チェック
+	var count int
+
+	err = db.Get(&count, "SELECT COUNT(*) FROM users WHERE Username=?", req.Username)
+	if err != nil {
+		return c.String(http.StatusInternalServerError, fmt.Sprintf("db error: %v", err))
+	}
+
+	if count > 0 {
+		return c.String(http.StatusConflict, "ユーザーが既に存在しています")
+	}
+
+	_, err = db.Exec("INSERT INTO users (Username, HashedPass) VALUES (?, ?)", req.Username, hashedPass)
+	if err != nil {
+		return c.String(http.StatusInternalServerError, fmt.Sprintf("db error: %v", err))
+	}
+	return c.NoContent(http.StatusCreated)
+}
+
+func postLoginHandler(c echo.Context) error {
+	req := LoginRequestBody{}
+	c.Bind(&req)
+
+	user := User{}
+	err := db.Get(&user, "SELECT * FROM users WHERE username=?", req.Username)
+	if err != nil {
+		return c.String(http.StatusInternalServerError, fmt.Sprintf("db error: %v", err))
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(user.HashedPass), []byte(req.Password))
+	if err != nil {
+		if err == bcrypt.ErrMismatchedHashAndPassword {
+			return c.NoContent(http.StatusForbidden)
+		} else {
+			return c.NoContent(http.StatusInternalServerError)
+		}
+	}
+
+	sess, err := session.Get("sessions", c)
+	if err != nil {
+		fmt.Println(err)
+		return c.String(http.StatusInternalServerError, "something wrong in getting session")
+	}
+	sess.Values["userName"] = req.Username
+	sess.Save(c.Request(), c.Response())
+
+	return c.NoContent(http.StatusOK)
+}
+
+func checkLogin(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		sess, err := session.Get("sessions", c)
+		if err != nil {
+			fmt.Println(err)
+			return c.String(http.StatusInternalServerError, "something wrong in getting session")
+		}
+
+		if sess.Values["userName"] == nil {
+			return c.String(http.StatusForbidden, "please login")
+		}
+		c.Set("userName", sess.Values["userName"].(string))
+
+		return next(c)
+	}
 }
 
 func getCityInfoHandler(c echo.Context) error {
 	cityName := c.Param("cityName")
-	fmt.Println(cityName)
 
 	city := City{}
 	db.Get(&city, "SELECT * FROM city WHERE Name=?", cityName)
@@ -68,36 +157,11 @@ func getCityInfoHandler(c echo.Context) error {
 		return c.NoContent(http.StatusNotFound)
 	}
 
-	return c.JSON(http.StatusOK, city)
+	return c.JSON(http.StatusOK, struct {
+		City     City
+		UserName interface{}
+	}{
+		city,
+		c.Get("userName"),
+	})
 }
-
-func addCityInfoHandler(c echo.Context) error {
-	var city City
-
-	if err := c.Bind(&city); err != nil {
-		return c.JSON(http.StatusBadRequest, city)
-	}
-	db.Exec(fmt.Sprintf("INSERT INTO city (Name, CountryCode, District, Population) VALUES ('%s', '%s', '%s', %d);", city.Name, city.CountryCode, city.District, city.Population))
-	return c.String(http.StatusOK, "Success")
-}
-
-// fmt.Println("Connected!")
-
-// city := City{}
-// country := Country{}
-// // db.Get(&city, "SELECT * FROM city WHERE Name='Tokyo'")
-// db.Get(&city, fmt.Sprintf("SELECT * FROM city WHERE Name='%s'", os.Args[1]))
-// db.Get(&country, fmt.Sprintf("SELECT * FROM country WHERE Code='%s'", city.CountryCode))
-
-// // fmt.Printf("%sの人口は%d人です\n", os.Args[1], city.Population)
-// fmt.Printf("%sの人口は%sの人口の%f%%です\n", city.Name, country.Name, float64(city.Population)/float64(country.Population)*100)
-
-// cities := []City{}
-// db.Select(&cities, "SELECT * FROM city WHERE CountryCode='JPN'")
-
-// fmt.Println("日本の都市一覧")
-// for _, city := range cities {
-// 	fmt.Printf("都市名: %s, 人口: %d人\n", city.Name, city.Population)
-// }
-
-// db.Exec("INSERT INTO city (Name, CountryCode, District, Population) VALUES ('oookayama', 'JPN', 'Tokyo', 2147483647);")
